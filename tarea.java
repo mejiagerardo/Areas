3fbe5874d15e6ff9204dbdd7842b085a1a0690de
mejiagerardo <student@Gerado-Mejia.local>
2014-11-18 15:35:05 -0600

    Agregue tarea
    es de cáliz para aprender a subir archivos a github

---
+++ tarea.java
@@ -0,0 +1,10 @@
+
+public class tarea {
+	
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		System.out.println("Gerardo Mejía Bravo \n A01176427 \n 17 años");
+		
+	}
+
+}
